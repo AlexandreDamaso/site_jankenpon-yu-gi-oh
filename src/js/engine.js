8db6ad6b44befd171da27dc626bab00cf4d7d913
Alexandre Damaso <alexandredamasocosta@gmail.com>
2024-12-05 16:35:24 -0300

    Criando a enumeração de cartas

--- src/js/engine.js
+++ src/js/engine.js
@@ -16,9 +16,38 @@ const state = {
    button: document.getElementById("next-duel"),
 };
 
-const players = {
-   player1: "player-cards"
-}
+const pathImages = "./src/icons/"
+
+const cardData = [
+   {
+      id: 0,
+      name: "Blue Eyes Ehite Dragon",
+      type: "Paper",
+      img: `${pathImages}dragon.png`,
+      WinOf: [1],
+      LoseOf: [2],
+   },
+   {
+      id: 1,
+      name: "Dark Magician",
+      type: "Rock",
+      img: `${pathImages}magician.png`,
+      WinOf: [2],
+      LoseOf: [0],
+   },
+   {
+      id: 2,
+      name: "Exodia",
+      type: "Scissors",
+      img: `${pathImages}exodia.png`,
+      WinOf: [0],
+      LoseOf: [1],
+   },
+]
+
+// const players = {
+//    player1: "player-cards"
+// }
 
 function init() {};
 
